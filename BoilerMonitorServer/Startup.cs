785e6e864fee037a15d2c51f39955606a9b03d34
tommi@rouvali.com <Tommi Rouvali>
2021-06-12 16:05:43 +0300

    x

--- BoilerMonitorServer/Startup.cs
+++ BoilerMonitorServer/Startup.cs
@@ -36,6 +36,8 @@ namespace BoilerMonitorServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -44,6 +46,7 @@ namespace BoilerMonitorServer
             {
                 app.UseExceptionHandler("/Error");
             }
+            */
 
             app.UseStaticFiles();
             if (!env.IsDevelopment())
