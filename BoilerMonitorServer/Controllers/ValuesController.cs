b194aa0655c13c13d57fcd3a55412aa91e1750b6
tommi@rouvali.com <Tommi Rouvali>
2021-06-21 08:57:14 +0300

    Fix

--- BoilerMonitorServer/Controllers/ValuesController.cs
+++ BoilerMonitorServer/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
 ﻿using BoilerMonitorServer.DBContext;
+using DTO;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -63,6 +64,47 @@ namespace BoilerMonitorServer.Controllers
         public Temperature Get()
         {
             return _context.Temperatures.Where(i => i.Point == 0).OrderByDescending(i => i.Time).Take(1).FirstOrDefault();
+        }
+
+        [HttpGet]
+        [Route("get12hData")]
+        public Trace[] Get12hData()
+        {
+
+            DateTime now = DateTime.UtcNow.FloorSeconds();
+            DateTime minus12 = DateTime.UtcNow.AddHours(-12);
+            DateTime[] times = new DateTime[12 * 60];
+            double[] values1 = new double[12 * 60];
+            double[] values2 = new double[12 * 60];
+
+            for (int i = 0; i < times.Length; i++)
+                times[i] = minus12.AddMinutes(i).ToLocalTime();
+
+            foreach (var m in _context.Temperatures.Where(i => i.Time > minus12).ToArray().GroupBy(t => new { t.Time }).Select(i => new
+            {
+                i.Key.Time,
+                Temp0 = i.Where(i => i.Point == 0).Average(p => p.Value),
+                Temp1 = i.Where(i => i.Point == 1).Average(p => p.Value),
+                Temp2 = i.Where(i => i.Point == 2).Average(p => p.Value),
+                Temp3 = i.Where(i => i.Point == 3).Average(p => p.Value)
+            }))
+            {
+                int index = (int)((m.Time.FloorSeconds() - minus12).TotalMinutes);
+                if (index > 0 && index < times.Length)
+                {
+                    values1[index] = m.Temp1;
+                    values2[index] = m.Temp2;
+                }
+            }
+
+            // Last slot is not always filled.
+            if (values1[values1.Length - 1] == 0)
+                values1[values1.Length - 1] = values1[values1.Length - 2];
+
+            if (values2[values2.Length - 1] == 0)
+                values2[values2.Length - 1] = values2[values2.Length - 2];
+
+            return new Trace[] { new Trace() { x = times, y = values1 }, new Trace() { x = times, y = values2 } };
         }
     }
 }
